cbdadfe9052e052d88cb97cfb37431427db1b339
Vinícius Ponte <108896825+ViniciusMPonte@users.noreply.github.com>
2024-08-23 23:03:02 +0000

    First Exercice

--- Main.java
+++ Main.java
@@ -3,24 +3,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    
-        int firstNum, secondNum, result;
         
         Scanner scanner = new Scanner(System.in);
+
+        double quantity1, quantity2, quantity3;
+        double value1, value2, value3;
+        double subTotal1, subTotal2, subTotal3;
+        double total;
+
+        System.out.print("\nDigite quantidade 1: ");
+        quantity1 = scanner.nextDouble();
+        System.out.print("\nDigite valor 1: ");
+        value1 = scanner.nextDouble();
+
+        System.out.print("\nDigite quantidade 2: ");
+        quantity2 = scanner.nextDouble();
+        System.out.print("\nDigite valor 2: ");
+        value2 = scanner.nextDouble();
+
+        System.out.print("\nDigite quantidade 3: ");
+        quantity3 = scanner.nextDouble();
+        System.out.print("\nDigite valor 3: ");
+        value3 = scanner.nextDouble();
+
+        subTotal1 = quantity1 * value1;
+        subTotal2 = quantity2 * value2;
+        subTotal3 = quantity3 * value3;
+        total = subTotal1 + subTotal2 + subTotal3;
+
+        System.out.println("\nSubtotal 1: " + subTotal1);
+        System.out.println("Subtotal 2: " + subTotal2);
+        System.out.println("Subtotal 3: " + subTotal3);
+        System.out.println("\nTotal: " + total);
         
-        System.out.print("Digite o primeiro Número: ");
-        
-        firstNum = scanner.nextInt();
-        
-        System.out.print("Digite o segundo Número: ");
-        
-        secondNum = scanner.nextInt();
-        
-        result = firstNum + secondNum;
-        
-        System.out.print("O resultado da soma é: " + result);
         scanner.close();
         
-}    
+    }    
 
 }
