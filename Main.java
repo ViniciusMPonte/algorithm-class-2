23635f35b74994f9605eaf1a4a3aeaae6a0dd3f0
Vinícius Ponte <108896825+ViniciusMPonte@users.noreply.github.com>
2024-08-23 22:48:30 +0000

    Test sync

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+    
+        int firstNum, secondNum, result;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o primeiro Número: ");
+        
+        firstNum = scanner.nextInt();
+        
+        System.out.print("Digite o segundo Número: ");
+        
+        secondNum = scanner.nextInt();
+        
+        result = firstNum + secondNum;
+        
+        System.out.print("O resultado da soma é: " + result);
+        scanner.close();
+        
+}    
+
 }
